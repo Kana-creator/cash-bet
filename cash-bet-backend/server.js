a90b1377230815a9c233f444a420c7ed531e28c1
Anatoli <akuwebwa@gmail.com>
2024-09-06 15:10:05 +0300

    Anatoli

--- cash-bet-backend/server.js
+++ cash-bet-backend/server.js
@@ -93,13 +93,13 @@ const dbConn = mysql.createConnection({
   port: process.env.DB_PORT,
 });
 
-const dbConn2 = mysql2.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD + "#",
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
+// const dbConn2 = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD + "#",
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+// });
 
 // GAMES API KEY
 const API_KEY = process.env.GAMES_API_KEY;
